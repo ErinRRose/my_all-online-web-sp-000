ec1e327f380fc29d8b458a443f20cf543ad22d87
Erin  Rose <errose10@gmail.com>
2019-11-11 01:58:13 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
-    yield(colletion[i])
+    block_return_values << yield(colletion[i])
     i += 1
   end
 end
